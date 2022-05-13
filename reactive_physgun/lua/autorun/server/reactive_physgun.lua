42336a7971afd80ae2f5f8a6b4c38677939dec3c
Piengineer12 <49790896+Piengineer12@users.noreply.github.com>
2022-05-14 00:33:56 +0800

    RPhys Version 0.3.0

--- reactive_physgun/lua/autorun/server/reactive_physgun.lua
+++ reactive_physgun/lua/autorun/server/reactive_physgun.lua
@@ -6,12 +6,20 @@ Donate:			https://ko-fi.com/piengineer12
 
 Links above are confirmed working as of 2022-05-12. All dates are in ISO 8601 format.
 
-Version:		0.2.0
+Version:		0.3.0
 ]]
 
 local flatMulConVar = CreateConVar("reactive_physgun_mul", "1", FCVAR_ARCHIVE, "Multiplies the amount of reactive force the Physics Gun applies. A value of 0 will disable reactive forces.")
 --local ppm2FlyMulConVar = CreateConVar("reactive_physgun_ponyflymul", "0.002", FCVAR_ARCHIVE, "Multiplies the amount of reactive force for flying CPPM or PPM/2 playermodels. Unused if neither are installed.")
 local maxMassFactorConVar = CreateConVar("reactive_physgun_maxmassfactor", "15", FCVAR_ARCHIVE, "Maximum multiplier of Physics Gun reactive forces due to object mass.")
+local forceAlgorithmConVar = CreateConVar("reactive_physgun_algorithm", "2", FCVAR_ARCHIVE,
+[[Sets the algorithm used to determine displacement of expected and actual points.
+0: No displacement
+1: Tick velocities
+2: Expected: tick velocity, Actual: point tick velocity
+3: Expected: tick velocity, Actual: dampened point tick velocity]])
+local usePredictiveWheelingConVar = CreateConVar("reactive_physgun_predictivewheeling", "0", FCVAR_ARCHIVE,
+"Extending / retracting props will adjust predicted distance based on input amount rather than being recalculated after moving the prop.")
 
 local plyReactivePhysguns = {}
 hook.Add("OnPhysgunPickup", "ReactivePhysgun", function(ply, ent)
@@ -36,33 +44,44 @@ hook.Add("PhysgunDrop", "ReactivePhysgun", function(ply, ent)
 end)
 
 hook.Add("Think", "ReactivePhysgun", function()
+	local timeToArrive = GetConVar("physgun_timeToArrive"):GetFloat()
+	local dampingFactor = GetConVar("physgun_DampingFactor"):GetFloat()
+	local forceAlgorithm = forceAlgorithmConVar:GetInt()
+	local frameTime = FrameTime()
 	for ply, data in pairs(plyReactivePhysguns) do
 		local ent = data.ent
 		if IsValid(ent) then
 			local aimVec = ply:GetAimVector()
 			local physObj = ent:GetPhysicsObject()
 			local shootPos = ply:GetShootPos()
-			local timeToArrive = GetConVar("physgun_timeToArrive"):GetFloat()
-			local dampingFactor = GetConVar("physgun_DampingFactor"):GetFloat()
-			local frameTime = FrameTime()
 			
 			local actualGrabPos = ent:LocalToWorld(data.localOffset)
-			local addVel = physObj:GetVelocityAtPoint(actualGrabPos)
-			addVel:Mul(timeToArrive*dampingFactor)
-			actualGrabPos:Add(addVel)
+			if forceAlgorithm >= 1 then
+				local addVel = forceAlgorithm >= 2 and physObj:GetVelocityAtPoint(actualGrabPos) or physObj:GetVelocity()
+				if forceAlgorithm >= 3 then
+					addVel:Mul(timeToArrive*dampingFactor)
+				else
+					addVel:Mul(frameTime)
+				end
+				actualGrabPos:Add(addVel)
+			end
 			
-			local expectedGrabPos = aimVec
-			local addVel = ply:GetVelocity()
-			addVel:Mul(frameTime)
-			addVel:Add(shootPos)
+			local startGrabPos = shootPos
+			if forceAlgorithm >= 1 then
+				local addVel = ply:GetVelocity()
+				addVel:Mul(frameTime)
+				startGrabPos:Add(addVel)
+			end
+			
 			if data.distanceRequiresReset then
-				local samplingPos = addVel
-				local distance = samplingPos:Distance(actualGrabPos)
+				local distance = startGrabPos:Distance(actualGrabPos)
 				data.distance = distance
 				data.distanceRequiresReset = false
 			end
+			
+			local expectedGrabPos = aimVec
 			expectedGrabPos:Mul(data.distance)
-			expectedGrabPos:Add(addVel)
+			expectedGrabPos:Add(startGrabPos)
 			
 			local force = actualGrabPos - expectedGrabPos
 			local massMul = math.Clamp(data.mass/ply:GetPhysicsObject():GetMass(), 0, maxMassFactorConVar:GetFloat())
@@ -121,34 +140,38 @@ hook.Add("PlayerTick", "ReactivePhysgun", function(ply, mv)
 	if data then
 		local keys = mv:GetButtons()
 		if bit.band(keys, payAttentionKeys) ~= 0 then
-			local outward = bit.band(keys, IN_WEAPON1) ~= 0
-			local inward = bit.band(keys, IN_WEAPON2) ~= 0
-			local use = bit.band(keys, IN_USE) ~= 0
-			local amount = GetConVar("physgun_wheelspeed"):GetFloat()
-			if outward ~= inward then
-				amount = amount * (outward and 1 or -1)
-				plyReactivePhysguns[ply].distance = plyReactivePhysguns[ply].distance + amount
-				--print(amount)
-			end
-			if use then
+			if usePredictiveWheelingConVar:GetBool() then
+				local outward = bit.band(keys, IN_WEAPON1) ~= 0
+				local inward = bit.band(keys, IN_WEAPON2) ~= 0
+				local use = bit.band(keys, IN_USE) ~= 0
+				local amount = GetConVar("physgun_wheelspeed"):GetFloat()
+				if outward ~= inward then
+					amount = amount * (outward and 1 or -1)
+					data.distance = data.distance + amount
+					--print(amount)
+				end
+				if use then
+					data.distanceRequiresReset = true
+					
+					--[[data.distance = data.ent:LocalToWorld(data.localOffset):Distance(ply:GetShootPos())
+					
+					local ent = data.ent
+					local aimVec = ply:GetAimVector()
+					local physObj = ent:GetPhysicsObject()
+					local shootPos = ply:GetShootPos()
+					local vel = physObj:GetVelocity()
+					
+					local expectedGrabPos = ent:LocalToWorld(data.localOffset)
+					--expectedGrabPos:Add(vel)
+					
+					local actualGrabPos = aimVec
+					actualGrabPos:Mul(data.distance)
+					actualGrabPos:Add(shootPos)
+					
+					--print(actualGrabPos - expectedGrabPos)]]
+				end
+			else
 				data.distanceRequiresReset = true
-				
-				--[[data.distance = data.ent:LocalToWorld(data.localOffset):Distance(ply:GetShootPos())
-				
-				local ent = data.ent
-				local aimVec = ply:GetAimVector()
-				local physObj = ent:GetPhysicsObject()
-				local shootPos = ply:GetShootPos()
-				local vel = physObj:GetVelocity()
-				
-				local expectedGrabPos = ent:LocalToWorld(data.localOffset)
-				--expectedGrabPos:Add(vel)
-				
-				local actualGrabPos = aimVec
-				actualGrabPos:Mul(data.distance)
-				actualGrabPos:Add(shootPos)
-				
-				--print(actualGrabPos - expectedGrabPos)]]
 			end
 		end
 	end
